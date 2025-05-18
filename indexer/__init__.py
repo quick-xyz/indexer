5a35a8e5b6f607f2718409d07d5ee85253b7b1c2
quick <quick.0xyz@gmail.com>
2025-05-17 20:41:48 -0400

    init experiment

--- indexer/__init__.py
+++ indexer/__init__.py
@@ -3,6 +3,7 @@
 from .config.config_manager import config
 from .decode.contracts.registry import contract_registry
 from .factory import ComponentFactory
+from .component_registry import registry
 
 def create_indexer(custom_config=None, config_file=None):
     
@@ -12,11 +13,18 @@ def create_indexer(custom_config=None, config_file=None):
         if custom_config:
             config.update_config(custom_config)
 
+    contract_manager = ComponentFactory.get_contract_manager()
+    rpc = ComponentFactory.get_rpc_client()
+    gcs_handler = ComponentFactory.get_gcs_handler()
+    block_decoder = ComponentFactory.get_block_decoder()
+    components = registry
+
     return {
         "config": config,
         "contract_registry": contract_registry,
-        "contract_manager": ComponentFactory.get_contract_manager(),
-        "rpc": ComponentFactory.get_rpc_client(),
-        "gcs_handler": ComponentFactory.get_gcs_handler(),
-        "block_decoder": ComponentFactory.get_block_decoder(),
+        "component_registry": components,
+        "contract_manager": contract_manager,
+        "rpc": rpc,
+        "gcs_handler": gcs_handler,
+        "block_decoder": block_decoder,
     }
